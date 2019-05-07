0ee217cceba5d9b9c22b3eeefc67cce52ef22ec9
hkoflax <hkoflax@users.noreply.github.com>
2019-05-07 03:42:42 -0400

    Add Scoped dependency

---
+++ ThrehouseAspNetCore/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ThrehouseAspNetCore.Models;
+using ThrehouseAspNetCore.Services;
+
+namespace ThrehouseAspNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ITestService _testService = null;
+
+        public HomeController(ITestService testservice)
+        {
+            _testService = testservice;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = _testService.GetAboutContent();
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ThrehouseAspNetCore/Services/TestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThrehouseAspNetCore.Services
+{
+    public class TestService : ITestService
+    {
+        public String GetAboutContent() 
+        {
+            return "Here is the about view Content from test service";
+        }
+    }
+}
